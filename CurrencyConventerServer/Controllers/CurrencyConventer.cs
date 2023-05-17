7aa7976c350727f9fbf899080fcaa493a6f7e74b
Diana Cold <dicold@ukr.net>
2023-05-17 15:00:03 +0200

    Adding server

---
+++ CurrencyConventerServer/Controllers/CurrencyConventer.cs
@@ -0,0 +1,125 @@
+﻿using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace CurrencyConventerServer.Controllers
+{
+    public class CurrencyConventer
+    {
+        static readonly Dictionary<int, string> OneDigitNumber = new()
+            {
+                {1, "one"},
+                {2, "two"},
+                {3, "three"},
+                {4, "four"},
+                {5, "five"},
+                {6, "six"},
+                {7, "seven"},
+                {8, "eight"},
+                {9, "nine"}
+            };
+
+        static readonly Dictionary<int, string> TeenNumber = new()
+        {
+                {0, "ten"},
+                {1, "eleven"},
+                {2, "twelve"},
+                {3, "thirteen"},
+                {4, "fourteen"},
+                {5, "fifteen"},
+                {6, "sixteen"},
+                {7, "seventeen"},
+                {8, "eighteen"},
+                {9, "nineteen"}
+            };
+
+        static readonly Dictionary<int, string> SecondDigitNumber = new()
+            {
+                {2, "twenty"},
+                {3, "thirty"},
+                {4, "fourty"},
+                {5, "fifty"},
+                {6, "sixty"},
+                {7, "seventy"},
+                {8, "eighty"},
+                {9, "ninety"}
+            };
+
+        static readonly string[] currencies = { "", " thousand", " million" };
+
+        public static string NumberIntoString(decimal number)
+        {
+            // integer part of the number (dollars)
+            int integerPart = (int)Math.Truncate(number);
+
+            // fraction part of the number (cents)
+            int fractionPart = (int)((number - integerPart) * 100);
+
+            var result = new StringBuilder();
+
+            string dollars = integerPart == 1 ? "dollar" : "dollars";
+
+            if (integerPart == 0)
+                result.Append("zero ");
+            else
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    int tmp = integerPart % 1000;
+                    if (tmp != 0)
+                        result.Insert(0, $"{ThreeDigitsNumberConverter(tmp)}{currencies[i]} ");
+                    integerPart /= 1000;
+                }
+            }
+            result.Append(dollars);
+
+            if (fractionPart != 0)
+            {
+                result.Append(" and ");
+                result.Append(ThreeDigitsNumberConverter(fractionPart));
+                result.Append(" cent");
+                if (fractionPart > 1)
+                    result.Append('s');
+            }
+            
+            return result.ToString();
+        }
+
+        private static string ThreeDigitsNumberConverter(int number)
+        {
+            int ones = number % 10;
+            int tens = (number / 10) % 10;
+            int hundreds = number / 100;
+
+            // if number is 1 digit only
+            if (tens == 0 && hundreds == 0)
+                return OneDigitNumber[ones];
+
+            var result = new StringBuilder();
+
+            // the number is 3 digits
+            if (hundreds > 0)
+                result.Append($"{OneDigitNumber[hundreds]} hundred ");
+
+            // last two digits is teen numbers: 11, 12, ... 19
+            if (tens == 1)
+            {
+                result.Append(TeenNumber[ones]);
+                return result.ToString();
+            }
+
+            // process the last two digits of the number
+            if (tens != 0)
+            {
+                result.Append(SecondDigitNumber[tens]);
+
+                if (ones != 0)
+                {
+                    result.Append('-');
+                    result.Append(OneDigitNumber[ones]);
+                }
+            }
+
+            return result.ToString();
+        }
+    }
+}
---
+++ CurrencyConventerServer/Controllers/CurrencyConventerController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing.Constraints;
+using System.Text;
+
+namespace CurrencyConventerServer.Controllers
+{
+    [ApiController]
+    [Route("api")]
+    public class CurrencyConventerController : ControllerBase
+    {
+        [HttpGet("Conventer")]
+        public string Get(string number)
+        {
+            var result = new StringBuilder();
+            number = number.Replace(" ", "").Replace(",", ".");
+
+            try
+            {
+                // check the correct input of number
+                var currencyNumber = Convert.ToDecimal(number);
+                if (currencyNumber > 999999999.99m)
+                    throw new Exception("Number is too big");
+
+                // if number is negative, result string should start with a word "minus"
+                if (currencyNumber < 0)
+                    result.Append("minus ");
+
+                return CurrencyConventer.NumberIntoString(currencyNumber);
+
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+    
+        static IEnumerable<string> ChunkFromEnd(string str, int chunkSize)
+        {
+            if (chunkSize >= str.Length)
+            {
+                yield return str;
+                yield break;
+            }
+
+            int startIndex = Math.Max(0, str.Length - chunkSize);
+            yield return str.Substring(startIndex);
+
+            while (startIndex > 0)
+            {
+                startIndex = Math.Max(0, startIndex - chunkSize);
+                int length = Math.Min(chunkSize, str.Length - startIndex);
+                yield return str.Substring(startIndex, length);
+            }
+        }
+    }
+}
